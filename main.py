39813491ad27e97508ef28d5a6145a98f71bb917
Parker Van Roy <parkervr@aol.com>
2019-09-27 22:42:40 -0400

    used quotes on render_template

--- main.py
+++ main.py
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 # [START gae_python37_app]
-from flask import Flask, render_template
+from flask import Flask, render_template, requests
+import requests, json
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -23,13 +24,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Return a friendly HTTP greeting."""
-    return render_template(index.html)
+  return render_template('index.html')
 
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+#if __name__ == '__main__':
+  # This is used when running locally only. When deploying to Google App
+  # Engine, a webserver process such as Gunicorn will serve the app. This
+  # can be configured by adding an `entrypoint` to app.yaml.
+  #app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
