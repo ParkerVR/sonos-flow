13f14bdc999fbd1f6582c181f919805616baa061
Parker Van Roy <parkervr@aol.com>
2019-09-27 22:57:47 -0400

    updated app.yaml

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # [START gae_python37_app]
-from flask import Flask, render_template, requests
+from flask import Flask, render_template
 import requests, json
 
 
@@ -21,15 +21,15 @@ import requests, json
 # called `app` in `main.py`.
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-  return render_template('index.html')
+  response = render_template('index.html')
+  return response
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
   # This is used when running locally only. When deploying to Google App
   # Engine, a webserver process such as Gunicorn will serve the app. This
   # can be configured by adding an `entrypoint` to app.yaml.
-  #app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
